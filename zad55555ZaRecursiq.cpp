e918abb5e7480d7c716d8c2ea6a5559a7587fff5
Martbul <99181339+Martbul@users.noreply.github.com>
2024-02-16 18:44:10 +0200

    zad5

--- zad55555ZaRecursiq.cpp
+++ zad55555ZaRecursiq.cpp
@@ -1,1 +1,35 @@
 
+#include <iostream>;
+
+using namespace std;
+
+bool isPrime(int n, int d=2){
+    if(n<2) return false;
+    if(n==2) return true;
+    if(n % d ==0) return false;
+    if(d * d >n) return true;
+    return isPrime(n,d+1);
+}
+
+
+int nextPrime(int n){
+    n++;
+    if(isPrime(n)==true){
+        return n;
+    }
+    return nextPrime(n);
+
+
+
+}
+
+
+
+int main(){
+    int n;
+    cin>>n;
+   cout<<nextPrime(n);
+    return 0;
+
+}
+
