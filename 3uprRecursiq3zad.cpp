3b6b6f6b7f675f07eb340c91ad8f8d39a3bcbf13
Martbul <99181339+Martbul@users.noreply.github.com>
2024-02-23 18:58:11 +0200

    doesnt work

---
+++ 3uprRecursiq3zad.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+string fromDecimalToBinary(int a, string b="")
+{
+    if(a ==0)
+    {
+
+        cout << b;
+        return "0";
+    }
+    a = a /2;
+    if((a /2) % 2 ==0)
+    {
+
+        b +="0";
+        cout<<b;
+        fromDecimalToBinary(a);
+
+    }
+    else
+    {
+        b +="1";
+        cout<<b;
+        fromDecimalToBinary(a);
+    }
+}
+
+
+void system(int n){
+    string numb;
+    for(int i =0; i<n; i++){
+    numb+=numb;
+    }
+    int b=1;
+    while (numb!= fromDecimalToBinary(b)){
+     b++;
+    }
+    cout<<b;
+}
+
+
+int main()
+{
+
+    int a;
+    cin >> a;
+    fromDecimalToBinary(a);
+    system(a);
+}
+
---
+++ 3uprRecursiqZad4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+bool evklidPrime(int a, int b){
+    string b = '';
+if(a == 0) return;
+a= a /2;
+if(a % 2 ==0) {
+
+    b+ a;
+}else{
+    b + a
+}
+
+
+
+}
+
+int main(){
+
+int n;
+cin >> n;
+evklidPrime(n);
+}
+
+
