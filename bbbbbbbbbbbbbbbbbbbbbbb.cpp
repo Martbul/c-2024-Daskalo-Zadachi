ef0038e6866f853521c547ab92643bcda08f2b38
Martbul <99181339+Martbul@users.noreply.github.com>
2024-04-19 18:08:47 +0300

    zad2

--- bbbbbbbbbbbbbbbbbbbbbbb.cpp
+++ bbbbbbbbbbbbbbbbbbbbbbb.cpp
@@ -3,18 +3,35 @@
 
 using namespace std;
 
-void read(string filemane){
-    string placeholder;
-ifstream MyReadFile(filemane);
-while(getline(MyReadFile, placeholder)){
-    cout << placeholder;
-}
+void read(string filemane, string filename2)
+{
+    string placeholder, placeholder2;
+    ifstream MyReadFile(filemane);
+    ifstream MyReadFile2(filename2);
+    while(getline(MyReadFile, placeholder))
+    {
+        while(getline(MyReadFile2, placeholder2))
+        {
+            if(placeholder2==placeholder)
+            {
+                cout << placeholder2<< endl;
+                break;
+            }
+            else if(placeholder2 != placeholder)
+            {
+                cout << " - " << placeholder<<endl;
+                cout << " + " << placeholder2<<endl;
+                break;
+            }
+        }
+    }
     MyReadFile.close();
 
 }
 
 
-int main(){
-read("new.txt");
+int main()
+{
+    read("new.txt", "old.txt");
 
 }
